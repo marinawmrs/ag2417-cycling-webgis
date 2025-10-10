576bfc1efccb4fc75ebba4de5d3d81703caec5b2
Johanna Schaefer <johanna.2.schaefer@uni-konstanz.de>
2025-10-11 00:55:00 +0200

    availabilty per hour of the day added

--- server/routes/parking_rating.js
+++ server/routes/parking_rating.js
@@ -39,5 +39,40 @@ module.exports = (pool) => {
     }
   });
 
+  router.get('/parkering_average', async (req, res) => {
+    const { parking_id, startHour, endHour } = req.query;
+    const start_int = parseInt(startHour);
+    const end_int = parseInt(endHour);
+    try {
+      const general = await pool.query(`
+        SELECT
+          AVG(safety_rating)::numeric(3,1) AS avg_safety,
+          AVG(vibe_rating)::numeric(3,1) AS avg_vibe
+        FROM cykelparkering_ratings
+        WHERE parking_id = $1
+          AND created_at > NOW() - INTERVAL '10 weeks'
+      `, [parking_id]);
+      
+      // Availability → timespecific
+      const timeSpecific = await pool.query(`
+        SELECT
+          AVG(availability_rating)::numeric(3,1) AS avg_availability
+        FROM cykelparkering_ratings
+        WHERE parking_id = $1
+          AND created_at > NOW() - INTERVAL '10 weeks'
+          AND EXTRACT(HOUR FROM created_at) BETWEEN $2 AND $3
+      `, [parking_id, start_int, end_int]);
+      //res.json(result.rows[0]);
+      res.json({
+        avg_vibe: general.rows[0].avg_vibe,
+        avg_safety:general.rows[0].avg_safety,
+        avg_availability: timeSpecific.rows[0].avg_availability
+      });
+    } catch (err) {
+      console.error('Error fetching averages:', err);
+      res.status(500).json({ error: 'Database error' });
+    }
+  });
+
   return router;
 };
